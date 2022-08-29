03331f1a22c0410579b7ec6b47eb748286ef835c
Ilya Slesar <ilyaslez@protonmail.com>
2022-08-29 12:02:27 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# autozippy
+
+## Description
+AutoZippy is an easy to use automatic (non-interactive) archival software, designed to help you automate backups. Archive creation is configured with a simple and intuitive .YAML preset. 
+
+For creating archives built-in library tarfile and 3rd-party library py7zr are used.
+
+### Limitations
+Currently AutoZippy supports only 7z and tar archives as 2 most popular formats. Tar archives currently do not support encryption due to a lack of non-interactive GnuPG assymetric encryption in python. Though, nothing prevents you from encrypting a tar archive inside a 7z one.
+
+## YAML Preset
+YAML presets consist of archives and their settings. Every setting can be specified either on a file-wide level or overriden for specific archive with the notable exclusion of timestamps - they currently can onle be specified on a preset-wide level. Aside from universal settings, like output directory, every format has its own specific settings, like algorithms, header encryption and compression level.
+
+Archives and their settings are specified under `archives`. There they are listed by their filenames and each has to contain at least one file under its own `files` section. 
+
+### Usage Examples
+> autozippy cloud.yaml photos.yaml example.yaml
+
+As many configs as you want can be provided. No other options are supported.
+
+Example config is provided under filename example.yaml 
+
+### Settings
+
+* `mode` - **must be specified** - archive format, can be either `7z` or `tar`
+* `outdir` - directory where archives are going to be saved. Default is working directory.
+* `root` - directory from which to start path inside of an archive. For example, /etc/nginx/sites-available/yoursite with the root specified as /etc/nginx is going to be saved as sites-available/yoursite in the archive. Default is start of file paths you specify in config.
+* `pass` - password
+
+#### File-wide Settings
+* `timestamp ` - (true/false) whether to add timestamp to a filename of the archive. False by default.
+* `custom_timestamp` - specify custom format for a timestamp. Default: %Y-%md-%d. See details [here](https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes)
+
+#### 7z Specific Settings
+* `algo` - compression algorithm. Can be either `lzma2`, `lzma`, `bzip2`, `ppmd`, `deflate` or `store` if no compression needed. LZMA2 by default.
+* `lvl` - **supported only with LZMA2** - compression effort level
+* `crypt_h` - **can only be used if pass is specified** - whether to encrypt file headers (names of files inside of the archive)
+
+#### TAR Specific Settings
+* `algo` - compression algorithm. Can be either `xz` (lzma), `gz` (gunzip) or `bz2` (bzip2). No compression by default.
+* `lvl` - **supported only with LZMA** - compression effort level
+
+## Contributing
+Program is far from complete and all help is welcome. You may also report bugs or submite feature requests but I can't guarantee timely responses or fixes. 
---
+++ autozippy.py
@@ -0,0 +1,128 @@
+import argparse
+from pathlib import Path
+import yaml
+import py7zr
+import tarfile
+import datetime
+import os
+
+algos_7z = {
+    'lzma2:': py7zr.FILTER_LZMA2,
+    'lzma': py7zr.FILTER_LZMA,
+    'bzip2': py7zr.FILTER_BZIP2,
+    'ppmd': py7zr.FILTER_PPMD,
+    'deflate': py7zr.FILTER_DEFLATE,
+    'copy': py7zr.FILTER_COPY
+}
+
+def encrypt_7z(archive_name, files, password = None, sp_args = None):
+    filters = list()
+    if 'algo' in sp_args:
+        filters.append({'id': algos_7z[sp_args['algo']]})
+    else:
+        filters.append({'id': py7zr.FILTER_LZMA2})
+    if 'lvl' in sp_args:
+        filters[-1]['preset'] = sp_args['lvl']
+
+    with py7zr.SevenZipFile(archive_name, 'w', password=str(password), filters=filters) as arch:
+        if 'crypt_h' in sp_args:
+            arch.set_encrypted_header(True)
+        for file in files:
+            if Path(file).is_dir():
+                arch.writeall(file)
+            elif Path(file).exists():
+                arch.write(file)
+            else:
+                raise RuntimeError(f'File {file} does not exist')
+
+
+def encrypt_tar(archive_name, files, password = None, sp_args = None):
+    if 'algo' in sp_args:
+        if sp_args['algo'] == 'lzma':
+            sp_args['algo'] = 'xz'
+        mode = 'w:' + sp_args['algo']
+        archive_name = archive_name.with_suffix('.tar.' + sp_args['algo'])
+    else:
+        mode = 'w'
+    
+    arch = tarfile.open(archive_name, mode=mode, preset=sp_args['lvl'] if 'lvl' in sp_args 
+                                                        and sp_args['algo'] == 'xz' else None)
+    for file in files:
+        if Path(file).exists():
+            arch.add(file)
+        else:
+            raise RuntimeError(f'File {file} does not exist')
+    arch.close()
+    
+
+
+def process_preset(preset):
+    for archive_name in preset['archives'].keys():
+        archive = preset['archives'][archive_name]
+        archive_path = Path(archive_name)
+
+        if 'mode' in archive:
+            mode = archive['mode']
+        elif 'mode' in preset:
+            mode = preset['mode']
+        else:
+            raise RuntimeError('Archival mode not specified')
+        
+        if 'pass' in archive:
+            password = archive['pass']
+        elif 'pass' in preset:
+            password = preset['pass']
+        else:
+            password = None
+        
+        if 'outdir' in archive:
+            archive_path = Path(archive['outdir'], archive_path)
+        elif 'outdir' in preset:
+            archive_path = Path(preset['outdir'], archive_path)
+        
+        if 'root' in archive:
+            t_dir = os.getcwd()
+            os.chdir(archive['root'])
+            archive['files'] = map(lambda path: Path(path).relative_to(os.getcwd()), archive['files'])
+        elif 'root' in preset:
+            t_dir = os.getcwd()
+            os.chdir(preset['root'])
+            archive['files'] = map(lambda path: Path(path).relative_to(os.getcwd()), archive['files'])
+
+        if 'timestamp' in preset:
+            if 'custom_timestamp' in preset and preset['custom_timestamp']:
+                archive_path = archive_path.with_name({archive_path.name} + ' ' + 
+                                                      {datetime.datetime.now().strftime(preset['custom_timestamp'])})
+            else:
+                archive_path = archive_path.with_name(archive_path.name + ' ' +
+                                                      datetime.datetime.now().strftime('%Y-%m-%d'))
+        
+        if mode == '7z':
+            encrypt_7z(archive_path.with_suffix('.7z'), archive['files'],
+                       password=password, sp_args=archive['7z'] if '7z' in archive
+                                                  else preset['7z'] if '7z' in preset
+                                                  else None)
+        elif mode == 'tar':
+            encrypt_tar(archive_path.with_suffix('.tar'), archive['files'],
+                       password=password, sp_args=archive['tar'] if 'tar' in archive
+                                                  else preset['tar'] if 'tar' in preset
+                                                  else None)
+        else:
+            raise RuntimeError('Unknown mode in config')
+        
+        if 't_dir' in locals():
+            os.chdir(t_dir)
+            del t_dir
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Create archives as described in YAML preset')
+    parser.add_argument('preset', nargs='+')
+    args = parser.parse_args()
+
+    for preset in args.preset:
+        with open(preset, 'r') as f:
+            process_preset(yaml.safe_load(f))
+    
+
+main()
